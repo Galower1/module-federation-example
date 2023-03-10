10c308ea6e8c03db678e82d43e2691eafe78c73e
Galower1 <ariosgalower@gmail.com>
2023-03-09 21:24:04 -0400

    exposing file

---
+++ craco.config.ts
@@ -0,0 +1,31 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  devServer: {
+    port: 3001,
+  },
+  webpack: {
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: "app1",
+          exposes: {
+            "./Button": "./src/components/Button",
+          },
+          filename: "remoteEntry.js",
+          shared: {
+            react: { singleton: true },
+            "react-dom": { singleton: true },
+          },
+        }),
+      ],
+    },
+    configure: (webpackConfig: any) => ({
+      ...webpackConfig,
+      output: {
+        ...webpackConfig.output,
+        publicPath: "auto",
+      },
+    }),
+  },
+};
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Remote button</button>;
+}
+
+export default Button;
